01f753e625505ea81aafbe058a29cd7f78046e74
Joe Bottigliero <joseph.bottigliero@punchkickinteractive.com>
2015-06-11 21:20:14 -0500

    Unit Tests + Fixes
    Added karma and qunit for some basic unit tests.
    Updated the extension to actually utilize `hammerjs` configuration on
    Backbone.View creation.

--- src/backbone-hammerjs-view.js
+++ src/backbone-hammerjs-view.js
@@ -1,7 +1,9 @@
 /**
  * Backbone.HammerJSView
+ * 
+ * (c) 2015 Joe Bottigliero
+ * Backbone.HammerJSView may be freely distributed under the MIT license.
  */
-
 (function(root, factory){
     if (typeof define === 'function' && define.amd) {
       define(['underscore', 'backbone'], factory);
@@ -12,22 +14,32 @@
   
   'use strict';
 
-  Backbone.HammerJSView = Backbone.View.extend({
-    /**
-     * Options to pass to the jQuery HammerJS plugin (Hammer construct)
-     * @type {null|object}
-     */
-    hammerjs: null,
+  var hammerViewOptions = ['hammerjs'];
 
-    /**
-     * Extends the functionality of Backbone's internal _setElement method
-     * to ensure that HammerJS is enabled on the view's element.
-     * @inheirt
-     */
-    _setElement: function() {
-      Backbone.View.prototype._setElement.apply(this, arguments);
-      this.$el.hammer(this.hammerjs || {});
+  Backbone.HammerJSView = function(options) {
+    _.extend(this, _.pick(options, hammerViewOptions));
+    Backbone.View.prototype.constructor.apply(this, arguments);
+  };
+
+  _.extend(
+    Backbone.HammerJSView.prototype,
+    Backbone.View.prototype,
+    {
+      /**
+       * Options to pass to the jQuery HammerJS plugin (Hammer construct)
+       * @type {null|object}
+       */
+      hammerjs: null,
+      /**
+       * Extends the functionality of Backbone's internal _setElement method
+       * to ensure that HammerJS is enabled on the view's element.
+       * @inheirt
+       */
+      _setElement: function() {
+        Backbone.View.prototype._setElement.apply(this, arguments);
+        this.$el.hammer(this.hammerjs || {});
+      }
     }
-  });
+  );
 
 }));
---
+++ tests/unit/backbone-hammerjs-view.js
@@ -0,0 +1,60 @@
+(function(){
+  'use strict';
+  test('$el.data(\'hammer\')', function(){
+
+    expect(1);
+
+    var view = new Backbone.HammerJSView();
+
+    view.setElement(document.createElement('div'));
+
+    ok(
+      view.$el.data('hammer') !== 'undefined',
+      'Hammer instance was found on the view element.'
+    );
+  });
+
+
+  test('tap', function(){
+
+    expect(1);
+
+    var view = new Backbone.HammerJSView({
+      events: {
+        tap: function(){ ok(true) }
+      }
+    });
+
+    view.setElement(document.createElement('div'));
+
+    view.$el.trigger('tap');
+  });
+
+  test('swipeup', function(){
+
+    expect(2);
+
+    var view = new Backbone.HammerJSView({
+      
+      hammerjs: {
+        recognizers: [
+          [Hammer.Swipe, { direction: Hammer.DIRECTION_UP }]
+        ]
+      },
+
+      events: {
+        swipeup   : function() { ok(true, 'The swipeup event was fired.') }
+      }
+    });
+
+    view.setElement(document.createElement('div'));
+    
+    equal(
+      view.$el.data('hammer').get('swipe').options.direction,
+      Hammer.DIRECTION_UP
+    );
+
+    view.$el.trigger('swipeup');
+  });
+
+})();
